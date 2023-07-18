f099a2854eece81ff35a0773542874aac4b1578e
David Sanchez <davidendubai@gmail.com>
2023-07-17 21:21:09 -0500

    ng g m protected --routing [+] ng g c protected/pages/dashboard [+]app-routing.module: Define ruta de modulo 'dashboard' [+] protected-routing.module: Define rutas hijas del modulo 'Dashboard'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,15 @@ import { RegisterComponent } from './components/register/register.component';
 const routes: Routes = [ 
   { path: '', component: HomeComponent },               //home   home page  http://localhost:4200/
   { path: 'login', component: LoginComponent },         //home page  http://localhost:4200/login
-  { path: 'register', component: RegisterComponent },   //http://localhost:4200/register
+  { path: 'register', component: RegisterComponent }, 
+  
+
+  { //Define ruta de modulo usando Lazyload (Carga Perezosa)
+    
+    path: 'dashboard',
+    loadChildren: () => import( './protected/protected.module' )
+                              .then( module => module.ProtectedModule )
+  },  //http://localhost:4200/register
   { path: '**', redirectTo: ''  }                       //http://localhost:4200/register
 ];
 
