0216bc69bf8110efc3e1f2685d6f6509d7513f49
David Sanchez <davidendubai@gmail.com>
2023-07-17 20:08:13 -0500

    [+] app.module: importamos ReactiveForms Module, login.component: Implementamos formularios reactivos (validacion)

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm: FormGroup = this.fb.group({
+    email: [ 
+      '',     //Valor predeterminado del campo
+     [        //Validaciones
+       Validators.required,
+       Validators.email
+     ]
+    ],
+    password:[
+      '',
+      [   //Validaciones
+        Validators.required,
+        Validators.minLength( 8 )
+      ]
+    ]
+  });
+
+/*Inyeccion de dependencias*/ 
+  constructor( private fb: FormBuilder ) {}
+
+  /**Login */
+  login(){
+    console.group ( 'loginForm' );
+    console.info( this.loginForm.value );
+    console.info( this.loginForm.valid );
+    
+    console.groupEnd();  
+  }
 }
